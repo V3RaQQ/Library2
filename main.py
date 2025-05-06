d61809fa1dc39cbbd6ca94a102da68bce3701e5a
V3RaQQ <ivandkorotich@gmai.com>
2025-05-06 21:16:43 +0300

    added removing

--- main.py
+++ main.py
@@ -5,13 +5,24 @@ from book import Book
 
 app = Flask(__name__)
 
-books = []
+
+books = [Book('name', 'fheiwo', 'fhjewuiof')]
+
 
 @app.route('/')
 def home():
     return render_template('index.html', books=books)
 
-
+@app.route("/remove/<name>")
+def remove(name):
+    if name in [book.name for book in books]:
+        for book in books:
+            if book.name == name:
+                books.remove(book)
+                print(f'removed book {book.name}')
+                return redirect(url_for('home'))
+    print('not finded')
+    return redirect(url_for('home'))
 
 
 
