14007e87cf355a255a11fd05c28d53fc796975f6
V3RaQQ <ivandkorotich@gmai.com>
2025-05-09 20:47:01 +0300

    added filter category

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect, url_for
 from book import Book
 
 
+d = {"a": 5, "b": 10}
+
 
 app = Flask(__name__)
 
@@ -17,14 +19,21 @@ print(categories)
 
 @app.route('/')
 def home():
+    # if request.args.get('books', False):
+    #     return render_template('index.html', books=request.args.get('books'), categories=categories)
+    if request.args.get('find_category'):
+        category = request.args.get('find_category')
+        print(category)
+        if category == 'null':
+            return render_template('index.html', books=books, categories=categories)
+        filtered = [book for book in books if book.category == category]
+        print(filtered)
+        return render_template('index.html', books=filtered, categories=categories)
     return render_template('index.html', books=books, categories=categories)
 
-@app.route('/add_book', methods=['POST'])
-def add_book():
-    pass
-
-
-
+# @app.route('/add_book', methods=['POST'])
+# def add_book():
+#     pass
 
 if __name__ == "__main__":
     app.run(debug=True)
