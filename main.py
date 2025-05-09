4d99ab7c54a36091388a044198239f2aed871719
V3RaQQ <ivandkorotich@gmai.com>
2025-05-09 20:12:37 +0300

    edited categories

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from book import Book
 
 app = Flask(__name__)
 
-books = []
+books = [Book("The Great Gatsby", "F. Scott Fitzgerald", "Fiction"),
+        Book("To Kill a Mockingbird", "Harper Lee", "Fiction"),
+        Book("1984", "George Orwell", "Dystopian")]
 
 @app.route('/')
 def home():
@@ -16,6 +18,8 @@ def add_book():
     name = request.form['name']
     author = request.form['author']
     category = request.form['category']
+    if category == 'null':
+        return render_template('index.html', books=books)
     new_book = Book(name, author, category)
     books.append(new_book)
     return redirect(url_for('home'))
