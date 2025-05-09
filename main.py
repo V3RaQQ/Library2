3088c4dbb611e0cc5eacf61be98e696b48475941
V3RaQQ <ivandkorotich@gmai.com>
2025-05-09 21:34:24 +0300

    added search

--- main.py
+++ main.py
@@ -5,14 +5,21 @@ from book import Book
 
 app = Flask(__name__)
 
-books = []
+books = [Book("Book1", "Author1", "Fiction"), 
+         Book("Book2", "Author2", "Non-Fiction"),
+         Book("Book3", "Author3", "Science"),
+         Book("Book4", "Author4", "History")]
 
 @app.route('/')
 def home():
+    if request.args.get('search_name'):
+        name = request.args.get('search_name')
+        return render_template('index.html', books=[book for book in books if book.name == name])
     return render_template('index.html', books=books)
 
-
-
+@app.route('/add_book')
+def add_book():
+    pass
 
 
 if __name__ == "__main__":
